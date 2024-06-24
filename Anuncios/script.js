3751133c19fe346bb2d03a3c6059d6598126de8c
Pedro <110565971+Pedrodek@users.noreply.github.com>
2024-06-24 11:13:07 -0300

    Add files via upload

---
+++ Anuncios/script.js
@@ -0,0 +1,12 @@
+$('.slicker-box').slick({
+    prevArrow: '<button type="button" class="prevArrow"><div>&#8592;</div></button>',
+    nextArrow: '<button type="button" class="nextArrow"><div>&#8594;</div></button>',
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
